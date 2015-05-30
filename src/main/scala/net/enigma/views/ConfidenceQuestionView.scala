ab76050376696f71f5d0656ff3990d4d872fa9af
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2015-05-30 23:40:32 +0200

    Changed ranges in confidence slider

--- src/main/scala/net/enigma/views/ConfidenceQuestionView.scala
+++ src/main/scala/net/enigma/views/ConfidenceQuestionView.scala
@@ -8,15 +8,16 @@ import net.enigma.Utils._
  * @author Jacek Lewandowski
  */
 trait ConfidenceQuestionView extends AbstractView {
-  val answerSlider = new Slider(question, 0, 100)
+  val answerSlider = new Slider(question, 50, 100)
       .withFullWidth
       .withImmediate
 
+  answerSlider.setValue(75d)
   answerSlider.setResolution(0)
 
   addInfo(instructions)
 
-  private val leftLabel = new Label("0%").withSizeUndefined
+  private val leftLabel = new Label("50%").withSizeUndefined
   private val rightLabel = new Label("100%").withSizeUndefined
   private val sliderLayout = new HorizontalLayout(leftLabel, answerSlider, rightLabel)
   sliderLayout
